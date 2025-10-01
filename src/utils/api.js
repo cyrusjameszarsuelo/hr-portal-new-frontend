ce2159e2b4a2186f31eb5fa1da677053cc39f7d6
cyrusjameszarsuelo <james.zarsuelo@indigo21.com>
2025-10-01 14:41:51 +0800

    api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with a default base URL
 const api = axios.create({
-  baseURL: 'http://192.168.100.125:8000/api', // Change this to your actual API base URL
+  baseURL: 'https://hr-portal-backend.portalwebsite.net/public/api', // Change this to your actual API base URL
 });
 
 export default api;
